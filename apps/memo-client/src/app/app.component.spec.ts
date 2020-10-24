02f12aabb32982df0e1b03f4580997e111bb175d
Ignacio Rojas Aguilar <imoon521@gmail.com>
2020-10-23 21:48:46 -0600

    Fix specs

--- apps/memo-client/src/app/app.component.spec.ts
+++ apps/memo-client/src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'memo-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('memo-client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to memo-client!'
-    );
-  });
 });
